c789651d2d020de23d9f8189c9827615e245d774
Riley Bathurst <riley@rileybathurst.com>
2021-07-22 18:30:54 -0700

    did this help the csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,16 +39,27 @@ module.exports = {
     },
     {
       resolve: `gatsby-plugin-csp`,
-        options: {
-          mergeStyleHashes: false, // you can disable styles sha256 hashes
-          mergeScriptHashes: false,
-          directives: {
-            "style-src": "'self' 'unsafe-inline'", // inline needed for gatsby and how it deals with things
-            // "script-src": "'self' 'unsafe-inline' use.typekit.net",
-            // the unsafe-inline on script-src is a problem for https://observatory.mozilla.org but without it the images break
-            // "font-src": "'self' 'unsafe-inline' use.typekit.net",
-          }
+      options: {
+        mergeStyleHashes: false, // you can disable styles sha256 hashes
+        mergeScriptHashes: false,
+        directives: {
+          "style-src": "'self' 'unsafe-inline'", // inline needed for gatsby and how it deals with things
+          "script-src": "'self' 'unsafe-inline'",
+          // the unsafe-inline on script-src is a problem for https://observatory.mozilla.org but without it the images break
+          // "font-src": "'self' 'unsafe-inline' use.typekit.net",
         }
+      }
+    },
+    // helps with the CSP in a substatial way
+    "gatsby-plugin-netlify",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: `Tahoe Web Shop`,
+        short_name: `TWS`,
+        start_url: `/`,
+        icon: "src/images/favicon.svg"
       },
+    },
   ],
 };
