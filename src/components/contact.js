19483ae7a690b338e77cffa69867ff5380751389
Riley Bathurst <riley@rileybathurst.com>
2021-08-13 10:06:47 -0700

    vs link

--- src/components/contact.js
+++ src/components/contact.js
@@ -30,7 +30,7 @@ const Contact = () => (
           <a href="mailto:info@tahoewebshop.com" className="button">info@tahoewebshop.com</a>
         </h3>
         <p>No form? Nope, I'm not a huge fan of them, I would prefer just a mail link, you can send from you preferred mail app, or service. This way you know if it works or not and we can just start your mail. I can still make you a form, let's just talk about it first.</p>
-
+        <p><a href="https://vanschneider.com/blog/down-with-contact-forms/" target='_blank' rel='noreferrer'>How about this article on the subject.</a></p>
       </div>
       <div>{/* stay gold */}</div>
     </article>
