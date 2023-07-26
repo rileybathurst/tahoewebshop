6b4a3b684f55d5a906733a5530f075e902d4e984
Riley <riley@rileybathurst.com>
2023-07-26 14:01:37 -0700

    ssr was in the wrong spit anyway

--- src/components/gatsby-ssr.tsx
+++
@@ -1,5 +0,0 @@
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({
-    lang: "en"
-  })
-}
--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -23,6 +23,7 @@ export const SEO = ({ title, description, pathname, children }) => {
 
   return (
     <>
+      <html lang="en" />
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       {children}
